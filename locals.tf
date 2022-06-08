f9cfc85c8484bdc4404bb127c2ccadc47df4fe07
Matt White <matt.white@microsoft.com>
2022-06-08 22:14:54 +0100

    Matt ffffff/initial functionality (#1)
    * set up workflows
    * set up module test framework
    * set up initial functionality

---
+++ locals.tf
@@ -0,0 +1,8 @@
+locals {
+  # subscription_id_alias is the id of the newly created subscription, if it exists.
+  subscription_id_alias = can(azapi_resource.subscription_alias[0].output) ? jsondecode(azapi_resource.subscription_alias[0].output).properties.subscriptionId : ""
+
+  # subscription_id is the id of the subscription into which resources will be created.
+  # We pick the created sub id first, if it exists, otherwise we pick the subscription_id variable.
+  subscription_id = coalesce(local.subscription_id_alias, var.subscription_id)
+}
