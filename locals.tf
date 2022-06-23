38ea70e235107008ceb0ca23a386f1da1d6623b6
Matt White <matt.white@microsoft.com>
2022-06-23 20:36:53 +0100

    Add output

--- locals.tf
+++ locals.tf
@@ -1,6 +1,15 @@
 locals {
   # subscription_id is the id of the subscription into which resources will be created.
   # We pick the created sub id first, if it exists, otherwise we pick the subscription_id variable.
-  # If this is blank then something has gone wrong.
-  subscription_id = can(coalesce(local.subscription_id_alias, var.subscription_id)) ? coalesce(local.subscription_id_alias, var.subscription_id) : ""
+  # If this is blank then the subscription submodule is disabled an no subscription id has been supplied as an input variabke.
+  subscription_id = coalesce(module.subscription.subscription_id, var.subscription_id, "")
+
+  # subscription_resource_id is the Azure resource id of the subscription id that was supplied in the inpuit variables.
+  # If var.subscription_id is empty, then we will return en empty string so that we can correctly coalesce the subscription_resource_id output.
+  supplied_subscription_resource_id = var.subscription_id == "" ? "" : "/subscriptions/${var.subscription_id}"
+
+  # subscription_resource_id is the Azure resource id of the subscription into which resources will be created.
+  # We use the created sub resoruce id first, if it exists, otherwise we pick the subscription_id variable.
+  # If this is blank then the subscription submodule is disabled an no subscription id has been supplied as an input variabke.
+  subscription_resource_id = coalesce(subscription.subscription_resource_id, local.supplied_subscription_resource_id, "")
 }
