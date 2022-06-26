68d8d45c89f81f7c5b70cae40751e6c88e8443d0
Matt White <matt.white@microsoft.com>
2022-06-26 21:56:31 +0100

    Test

---
+++ testazurerm/main.tf
@@ -0,0 +1,14 @@
+resource "azapi_resource" "rg" {
+  type      = "Microsoft.Resources/resourceGroups@2021-04-01"
+  parent_id = "/subscriptions/fd4423da-0d2c-480f-a55e-5453db019939"
+  name      = "deploytest-6754asd3"
+  location  = "westeurope"
+}
+
+resource "azurerm_public_ip" {
+  name                         = "deploytest-6754asd3-pip"
+  location                     = "westeurope"
+  resource_group_name          = azapi_resource.rg.name
+  public_ip_address_allocation = "Static"
+  sku                          = "Standard"
+}
---
+++ testazurerm/terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = ">= 0.3.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
