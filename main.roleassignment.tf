681f53c3fe621f55f468c829c08d07655cffa5a1
Matt White <matt.white@microsoft.com>
2022-07-13 09:26:53 +0100

    Add depends_on

--- main.roleassignment.tf
+++ main.roleassignment.tf
@@ -1,5 +1,12 @@
+# The roleassignments module creates role assignments from the data
+# supplied in the var.role_assignments variable.
+# var.role_assignment_enabled must also be set to true.
 module "roleassignment" {
   source                       = "./modules/roleassignment"
+  depends_on = [
+    module.subscription,
+    module.virtualnetwork,
+  ]
   for_each                     = local.role_assignments_map
   role_assignment_principal_id = each.value.principal_id
   role_assignment_definition   = each.value.definition
