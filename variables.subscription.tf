0e06988de88ea98cc8961b24175bcc76239e2b80
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2022-09-02 17:08:21 +0100

    docs: clarify billing scope and minor updates (#93)
    * docs: refactor examples and fix link
    
    * docs: add detail to subscription_billing_scope
    
    * fix: clarify docs to fix Bug Report #90

--- variables.subscription.tf
+++ variables.subscription.tf
@@ -56,6 +56,12 @@ The billing scope for the new subscription alias.
 
 A valid billing scope starts with `/providers/Microsoft.Billing/billingAccounts/` and is case sensitive.
 
+E.g.
+
+- For CustomerLed and FieldLed, e.g. MCA - `/providers/Microsoft.Billing/billingAccounts/{billingAccountName}/billingProfiles/{billingProfileName}/invoiceSections/{invoiceSectionName}`
+- For PartnerLed, e.g. MPA - `/providers/Microsoft.Billing/billingAccounts/{billingAccountName}/customers/{customerName}`
+- For Legacy EA - `/providers/Microsoft.Billing/billingAccounts/{billingAccountName}/enrollmentAccounts/{enrollmentAccountName}`
+
 You may also supply an empty string if you do not want to create a new subscription alias.
 In this scenario, `subscription_enabled` should be set to `false` and `subscription_id` must be supplied.
 DESCRIPTION
