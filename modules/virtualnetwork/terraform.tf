7e7f8840e71cf1c5c678bb58aa0da8168342afb0
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2022-10-14 21:58:40 +0100

    chore: update minimum azapi and tf versions

--- modules/virtualnetwork/terraform.tf
+++ modules/virtualnetwork/terraform.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.3.0"
   required_providers {
     azapi = {
       source  = "azure/azapi"
-      version = ">= 0.3.0"
+      version = ">= 1.0.0"
     }
   }
 }
