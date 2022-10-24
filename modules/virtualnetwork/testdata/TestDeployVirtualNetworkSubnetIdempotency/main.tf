1c84e3a2d9c31359b4992b37f9256d0acd38c152
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2022-10-24 15:15:32 +0100

    test: pass TestDeployVirtualNetworkValidVhubConnection

--- modules/virtualnetwork/testdata/TestDeployVirtualNetworkSubnetIdempotency/main.tf
+++ modules/virtualnetwork/testdata/TestDeployVirtualNetworkSubnetIdempotency/main.tf
@@ -1,5 +1,5 @@
 resource "azapi_resource" "subnet" {
-  parent_id = module.virtualnetwork_test.virtual_network_resource_id
+  parent_id = module.virtualnetwork_test.virtual_network_resource_ids["primary"]
   type      = "Microsoft.Network/virtualNetworks/subnets@2021-08-01"
   name      = "vnet-subnet"
   body = jsonencode({
@@ -10,11 +10,7 @@ resource "azapi_resource" "subnet" {
 }
 
 module "virtualnetwork_test" {
-  source                                = "../../"
-  subscription_id                       = var.subscription_id
-  virtual_network_address_space         = var.virtual_network_address_space
-  virtual_network_location              = var.virtual_network_location
-  virtual_network_resource_group_name   = var.virtual_network_resource_group_name
-  virtual_network_name                  = var.virtual_network_name
-  virtual_network_resource_lock_enabled = var.virtual_network_resource_lock_enabled
+  source           = "../../"
+  subscription_id  = var.subscription_id
+  virtual_networks = var.virtual_networks
 }
