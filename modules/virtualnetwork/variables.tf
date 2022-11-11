02bcaff2b49978c3e26ce309cffd001e1d7fd879
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2022-11-11 09:59:01 +0000

    Update modules/virtualnetwork/variables.tf
    Co-authored-by: Jack Tracey <41163455+jtracey93@users.noreply.github.com>

--- modules/virtualnetwork/variables.tf
+++ modules/virtualnetwork/variables.tf
@@ -92,7 +92,7 @@ A map of the virtual networks to create. The map key must be known at the plan s
 
 The following values configure bi-directional hub & spoke peering for the given virtual network.
 
-- `hub_peering_enabled`: Whether to enable hub peering. [optional - default false]
+- `hub_peering_enabled`: Whether to enable hub peering. [optional - default `false`]
 - `hub_network_resource_id`: The resource ID of the hub network to peer with. [optional - but required if hub_peering_enabled is `true`]
 - `hub_peering_name_tohub`: The name of the peering to the hub network. [optional - leave empty to use calculated name]
 - `hub_peering_name_fromhub`: The name of the peering from the hub network. [optional - leave empty to use calculated name]
