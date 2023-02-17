52e2433121b768b562e71567a3a9533aeca1e196
Kashif Saadat <kashifsaadat@gmail.com>
2023-02-17 14:22:12 +0000

    feat: create an output for management_group_subscription_association_id

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,11 @@ output "virtual_network_resource_ids" {
   value       = local.virtual_network_resource_ids
   description = "A map of virtual network resource ids, keyed by the var.virtual_networks input map. Only populated if the virtualnetwork submodule is enabled."
 }
+
+output "management_group_subscription_association_id" {
+  value       = var.subscription_management_group_association_enabled ? module.subscription[0].management_group_subscription_association_id : null
+  description = <<DESCRIPTION
+The management_group_subscription_association_id output is the ID of the management group subscription association.
+Value will be null if `var.subscription_management_group_association_enabled` is false.
+DESCRIPTION
+}
