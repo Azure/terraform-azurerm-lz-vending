c692b9e0eca0feeb1bd02d2665d6bbef256d27a7
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-06-23 12:44:20 +0100

    style: var naming

--- modules/networkwatcherrg/main.tf
+++ modules/networkwatcherrg/main.tf
@@ -1,7 +1,7 @@
 resource "azapi_resource" "network_watcher_rg" {
   parent_id = "/subscriptions/${var.subscription_id}"
   type      = "Microsoft.Resources/resourceGroups@2021-04-01"
-  name      = var.networkwatcher_rg_name
+  name      = var.network_watcher_rg_name
   location  = var.location
   body      = jsonencode({})
   tags      = var.tags
