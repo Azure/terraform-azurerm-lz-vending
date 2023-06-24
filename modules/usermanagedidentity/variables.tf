525152d78f47eea29f96fd4c7ff03ceb897fe691
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-06-24 11:45:49 +0100

    feat: add rg & location vars

--- modules/usermanagedidentity/variables.tf
+++ modules/usermanagedidentity/variables.tf
@@ -3,6 +3,18 @@ variable "name" {
   type        = string
 }
 
+variable "location" {
+  description = "The location of the user managed identity"
+  type        = string
+
+}
+
+variable "resource_group_creation_enabled" {
+  description = "Whether to create the supplied resource group"
+  type        = bool
+  default     = true
+}
+
 variable "resource_group_name" {
   description = "The name of the resource group in which to create the user managed identity"
   type        = string
