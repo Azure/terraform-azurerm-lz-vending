98f0551b59d1ccc9149537be0c0db1cc6b765851
Koen Rottiers <koen.rottiers@microsoft.com>
2023-07-11 14:45:07 +0200

    corrected variable reference

--- modules/virtualnetwork/main.tf
+++ modules/virtualnetwork/main.tf
@@ -167,7 +167,7 @@ resource "azapi_resource" "vhubconnection" {
       remoteVirtualNetwork = {
         id = local.virtual_network_resource_ids[each.key]
       }
-      routingConfiguration = each.value.vwan_routing_intent_enabled ? null : {
+      routingConfiguration = each.value.vwan_security_configuration.vwan_routing_intent_enabled ? null : {
           associatedRouteTable = {
             id = each.value.vwan_associated_routetable_resource_id != "" ? each.value.vwan_associated_routetable_resource_id : "${each.value.vwan_hub_resource_id}/hubRouteTables/defaultRouteTable"
           }
