09bb4f8d7de5ee950159da9dc565cc8ff0ac4d19
Dan Watkinson <dan.watco@gmail.com>
2023-08-03 09:06:37 +0100

    Added deployment test

---
+++ modules/subscription/testdata/TestDeploySubscriptionAliasAZAPIManagementGroupValid/main.tf
@@ -0,0 +1,53 @@
+variable "subscription_billing_scope" {
+  type = string
+}
+
+variable "subscription_management_group_id" {
+  type = string
+}
+
+variable "subscription_alias_name" {
+  type = string
+}
+
+variable "subscription_display_name" {
+  type = string
+}
+
+variable "subscription_workload" {
+  type = string
+}
+
+variable "subscription_management_group_association_enabled" {
+  type = bool
+}
+
+variable "subscription_alias_enabled" {
+  type = bool
+}
+
+variable "subscription_use_azapi" {
+  type = bool
+}
+
+resource "azapi_resource" "mg" {
+  type      = "Microsoft.Management/managementGroups@2021-04-01"
+  parent_id = "/"
+  name      = var.subscription_management_group_id
+}
+
+module "subscription_test" {
+  source                                            = "../../"
+  subscription_alias_name                           = var.subscription_alias_name
+  subscription_display_name                         = var.subscription_display_name
+  subscription_workload                             = var.subscription_workload
+  subscription_management_group_id                  = azapi_resource.mg.name
+  subscription_billing_scope                        = var.subscription_billing_scope
+  subscription_management_group_association_enabled = var.subscription_management_group_association_enabled
+  subscription_alias_enabled                        = var.subscription_alias_enabled
+  subscription_use_azapi                            = var.subscription_use_azapi
+}
+
+output "subscription_id" {
+  value = module.subscription_test.subscription_id
+}
