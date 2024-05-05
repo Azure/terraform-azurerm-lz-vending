5ae86a78644f0bccd91376fd867657d42488dd68
kewalaka <kewalaka@gmail.com>
2024-05-05 14:26:30 +1200

    fix: required provider

--- modules/routetable/terraform.tf
+++ modules/routetable/terraform.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.94"
+    azapi = {
+      source  = "azure/azapi"
+      version = ">= 1.11.0"
     }
   }
   required_version = ">= 1.5.0"
