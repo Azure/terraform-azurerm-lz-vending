8c3378595e1ea35c9202bfaf195b81d695c274ed
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2025-01-20 12:56:15 +0000

    chore: lint

--- variables.routetable.tf
+++ variables.routetable.tf
@@ -28,10 +28,10 @@ A map defining route tables and their associated routes to be created:
 - `bgp_route_propagation_enabled` (optional): Boolean that controls whether routes learned by BGP are propagated to the route table. Default is `true`.
 - `tags` (optional): A map of key-value pairs for tags associated with the route table.
 - `routes` (optional): A map defining routes for the route table. Each route object has the following properties:
-    - `name` (required): The name of the route.
-    - `address_prefix` (required): The address prefix for the route.
-    - `next_hop_type` (required): The type of next hop for the route.
-    - `next_hop_in_ip_address` (required): The next hop IP address for the route.
+  - `name` (required): The name of the route.
+  - `address_prefix` (required): The address prefix for the route.
+  - `next_hop_type` (required): The type of next hop for the route.
+  - `next_hop_in_ip_address` (required): The next hop IP address for the route.
 DESCRIPTION
   nullable    = false
   default     = {}
