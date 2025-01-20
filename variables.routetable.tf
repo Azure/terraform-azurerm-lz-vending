913325eb067f7a3d921cc5d4dca9f1d129026743
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2025-01-20 12:53:07 +0000

    chore: lint fixes

--- variables.routetable.tf
+++ variables.routetable.tf
@@ -20,17 +20,18 @@ variable "route_tables" {
     })))
   }))
   description = <<DESCRIPTION
-A map defining route tables and their associated routes to be created.
-  - `name` (required): The name of the route table.
-  - `location` (required): The location of the resource group.
-  - `resource_group_name` (required): The name of the resource group.
-  - `bgp_route_propagation_enabled` (optional): Boolean that controls whether routes learned by BGP are propagated to the route table. Default is `true`.
-  - `tags` (optional): A map of key-value pairs for tags associated with the route table.
-  - `routes` (optional): A map defining routes for the route table. Each route object has the following properties:
-      - `name` (required): The name of the route.
-      - `address_prefix` (required): The address prefix for the route.
-      - `next_hop_type` (required): The type of next hop for the route.
-      - `next_hop_in_ip_address` (required): The next hop IP address for the route.
+A map defining route tables and their associated routes to be created:
+
+- `name` (required): The name of the route table.
+- `location` (required): The location of the resource group.
+- `resource_group_name` (required): The name of the resource group.
+- `bgp_route_propagation_enabled` (optional): Boolean that controls whether routes learned by BGP are propagated to the route table. Default is `true`.
+- `tags` (optional): A map of key-value pairs for tags associated with the route table.
+- `routes` (optional): A map defining routes for the route table. Each route object has the following properties:
+    - `name` (required): The name of the route.
+    - `address_prefix` (required): The address prefix for the route.
+    - `next_hop_type` (required): The type of next hop for the route.
+    - `next_hop_in_ip_address` (required): The next hop IP address for the route.
 DESCRIPTION
   nullable    = false
   default     = {}
