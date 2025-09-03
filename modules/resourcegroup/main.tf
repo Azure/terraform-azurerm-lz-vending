933845e3eac3bff8af6f6fc08c66c43ef85af200
Remo Leone Laudo <remoleone@icloud.com>
2025-09-03 16:22:06 -0400

    Grabbed screenshot of body error for migration

--- modules/resourcegroup/main.tf
+++ modules/resourcegroup/main.tf
@@ -1,6 +1,6 @@
 resource "azapi_resource" "rg" {
   type      = "Microsoft.Resources/resourceGroups@2021-04-01"
-  body      = {}
+  
   location  = var.location
   name      = var.resource_group_name
   parent_id = "/subscriptions/${var.subscription_id}"
