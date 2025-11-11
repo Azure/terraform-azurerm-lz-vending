d5d392984ad9aef1e412c6ce24bc40e52df0bc8d
Jared Holgate <jaredholgate@microsoft.com>
2025-11-11 08:44:23 +0000

    update file name

--- tests/unit/setup.sh
+++ tests/unit/setup.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/usr/bin/bash
 
-echo "Copy the provider.tf file to the root module directory"
-cp ./tests/unit/setup/provider.tf ./provider.tf
+echo "Copy the providers.tf file to the root module directory"
+cp ./tests/unit/setup/providers.tf ./providers.tf
